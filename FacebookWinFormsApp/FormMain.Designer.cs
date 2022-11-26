e699c29262567ca5875b2a73d916dc767ad9fa47
shanikrauss <70583407+shanikrauss@users.noreply.github.com>
2022-11-26 16:29:34 +0200

    add files arbel

--- FacebookWinFormsApp/FormMain.Designer.cs
+++ FacebookWinFormsApp/FormMain.Designer.cs
@@ -28,41 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.buttonLogin = new System.Windows.Forms.Button();
-			this.buttonLogout = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// buttonLogin
-			// 
-			this.buttonLogin.Location = new System.Drawing.Point(12, 12);
-			this.buttonLogin.Name = "buttonLogin";
-			this.buttonLogin.Size = new System.Drawing.Size(179, 23);
-			this.buttonLogin.TabIndex = 36;
-			this.buttonLogin.Text = "Login";
-			this.buttonLogin.UseVisualStyleBackColor = true;
-			this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
-			// 
-			// buttonLogout
-			// 
-			this.buttonLogout.Location = new System.Drawing.Point(12, 41);
-			this.buttonLogout.Name = "buttonLogout";
-			this.buttonLogout.Size = new System.Drawing.Size(179, 23);
-			this.buttonLogout.TabIndex = 52;
-			this.buttonLogout.Text = "Logout";
-			this.buttonLogout.UseVisualStyleBackColor = true;
-			this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
-			// 
-			// FormMain
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(842, 657);
-			this.Controls.Add(this.buttonLogout);
-			this.Controls.Add(this.buttonLogin);
-			this.Name = "FormMain";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Form1";
-			this.ResumeLayout(false);
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.pictureBoxProfile = new System.Windows.Forms.PictureBox();
+            this.listBoxAboutInfo = new System.Windows.Forms.ListBox();
+            this.labelAbout = new System.Windows.Forms.Label();
+            this.listBoxPages = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAstrologyHoroscopePost = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(1024, 12);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(226, 35);
+            this.buttonLogin.TabIndex = 36;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Location = new System.Drawing.Point(1024, 51);
+            this.buttonLogout.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(226, 35);
+            this.buttonLogout.TabIndex = 52;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // pictureBoxProfile
+            // 
+            this.pictureBoxProfile.Location = new System.Drawing.Point(86, 12);
+            this.pictureBoxProfile.Name = "pictureBoxProfile";
+            this.pictureBoxProfile.Size = new System.Drawing.Size(157, 142);
+            this.pictureBoxProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxProfile.TabIndex = 53;
+            this.pictureBoxProfile.TabStop = false;
+            this.pictureBoxProfile.Click += new System.EventHandler(this.pictureBoxProfile_Click);
+            // 
+            // listBoxAboutInfo
+            // 
+            this.listBoxAboutInfo.FormattingEnabled = true;
+            this.listBoxAboutInfo.ItemHeight = 20;
+            this.listBoxAboutInfo.Location = new System.Drawing.Point(12, 198);
+            this.listBoxAboutInfo.Name = "listBoxAboutInfo";
+            this.listBoxAboutInfo.Size = new System.Drawing.Size(302, 84);
+            this.listBoxAboutInfo.TabIndex = 55;
+            this.listBoxAboutInfo.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // labelAbout
+            // 
+            this.labelAbout.AutoSize = true;
+            this.labelAbout.Location = new System.Drawing.Point(12, 175);
+            this.labelAbout.Name = "labelAbout";
+            this.labelAbout.Size = new System.Drawing.Size(56, 20);
+            this.labelAbout.TabIndex = 56;
+            this.labelAbout.Text = "About:";
+            this.labelAbout.Click += new System.EventHandler(this.labelAbout_Click);
+            // 
+            // listBoxPages
+            // 
+            this.listBoxPages.FormattingEnabled = true;
+            this.listBoxPages.ItemHeight = 20;
+            this.listBoxPages.Location = new System.Drawing.Point(12, 324);
+            this.listBoxPages.Name = "listBoxPages";
+            this.listBoxPages.Size = new System.Drawing.Size(302, 164);
+            this.listBoxPages.TabIndex = 57;
+            this.listBoxPages.SelectedIndexChanged += new System.EventHandler(this.listBoxPages_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 298);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 58;
+            this.label1.Text = "Liked pages";
+            // 
+            // buttonAstrologyHoroscopePost
+            // 
+            this.buttonAstrologyHoroscopePost.Location = new System.Drawing.Point(1073, 107);
+            this.buttonAstrologyHoroscopePost.Name = "buttonAstrologyHoroscopePost";
+            this.buttonAstrologyHoroscopePost.Size = new System.Drawing.Size(125, 60);
+            this.buttonAstrologyHoroscopePost.TabIndex = 59;
+            this.buttonAstrologyHoroscopePost.Text = "Post Astrology Horoscope";
+            this.buttonAstrologyHoroscopePost.UseVisualStyleBackColor = true;
+            this.buttonAstrologyHoroscopePost.Click += new System.EventHandler(this.buttonAstrologyHoroscopePost_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1263, 1011);
+            this.Controls.Add(this.buttonAstrologyHoroscopePost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxPages);
+            this.Controls.Add(this.labelAbout);
+            this.Controls.Add(this.listBoxAboutInfo);
+            this.Controls.Add(this.pictureBoxProfile);
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.buttonLogin);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +147,12 @@
 
 		private System.Windows.Forms.Button buttonLogin;
 		private System.Windows.Forms.Button buttonLogout;
-	}
+        private System.Windows.Forms.PictureBox pictureBoxProfile;
+        private System.Windows.Forms.ListBox listBoxAboutInfo;
+        private System.Windows.Forms.Label labelAbout;
+        private System.Windows.Forms.ListBox listBoxPages;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAstrologyHoroscopePost;
+    }
 }
 
--- FacebookWinFormsApp/FormMain.cs
+++ FacebookWinFormsApp/FormMain.cs
@@ -7,12 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using FacebookWrapper.ObjectModel;
+using FacebookAppLogic;
 using FacebookWrapper;
 
 namespace BasicFacebookFeatures
 {
     public partial class FormMain : Form
     {
+        private User m_LoggedInUser;
+        private LoginResult m_LoginResult;
+        private AstrologyHoroscope m_AstrologyHoroscope;
+
         public FormMain()
         {
             InitializeComponent();
@@ -23,22 +28,115 @@ namespace BasicFacebookFeatures
         {
             Clipboard.SetText("design.patterns20cc"); /// the current password for Desig Patter
 
-            FacebookWrapper.LoginResult loginResult = FacebookService.Login(
-                    /// (This is Desig Patter's App ID. replace it with your own)
-                    "1450160541956417", 
-                    /// requested permissions:
-					"email",
-                    "public_profile"
-                    /// add any relevant permissions
-                    );
+            m_LoginResult = FacebookService.Login(
+                    "1089208135099959",
+                    "email",
+                    "public_profile",
+                    "user_age_range",
+                    "user_birthday",
+                    "user_events",
+                    "user_friends",
+                    "user_gender",
+                    "user_hometown",
+                    "user_likes",
+                    "user_link",
+                    "user_location",
+                    "user_photos",
+                    "user_posts",
+                    "user_videos");
 
-            buttonLogin.Text = $"Logged in as {loginResult.LoggedInUser.Name}";
+            if (!string.IsNullOrEmpty(m_LoginResult.AccessToken))
+            {
+                m_LoggedInUser = m_LoginResult.LoggedInUser;
+                loadUserInfo();
+            }
+            else
+            {
+                MessageBox.Show(m_LoginResult.ErrorMessage, "Login Failed");
+            }
+
+           // buttonLogin.Text = $"Logged in as {m_LoginResult.LoggedInUser.Name}";
         }
+
+        private void loadUserInfo()
+        {
+            string userGender = m_LoggedInUser.Gender == User.eGender.female ? "Female" : "Male";
+            pictureBoxProfile.LoadAsync(m_LoggedInUser.PictureNormalURL);
+            listBoxAboutInfo.Items.Add(m_LoggedInUser.Email);
+            listBoxAboutInfo.Items.Add(m_LoggedInUser.Birthday);
+            listBoxAboutInfo.Items.Add(userGender);
+
+            //copied from Guy TODO: delete comment and change the code
+            listBoxPages.Items.Clear();
+            listBoxPages.DisplayMember = "Name";
+
+            try
+            {
+                foreach (Page page in m_LoggedInUser.LikedPages)
+                {
+                    listBoxPages.Items.Add(page);
+                }
+            }
+            catch (Exception ex)
+            {
+                 MessageBox.Show(ex.Message);
+            }
+
+            if (listBoxPages.Items.Count == 0)
+            {
+                MessageBox.Show("No liked pages to retrieve :(");
+            }
+            ///
+        }
+
 
         private void buttonLogout_Click(object sender, EventArgs e)
         {
 			FacebookService.LogoutWithUI();
 			buttonLogin.Text = "Login";
-		}
-	}
+            m_LoginResult = null;
+        }
+
+        private void pictureBoxProfile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listViewAboutInfo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelAbout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxPages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void buttonAstrologyHoroscopePost_Click(object sender, EventArgs e)
+        {
+            //copied from Guy TODO: delete comment and change the code
+            try
+            {
+                string astrologyHoroscopePost = await m_AstrologyHoroscope.CreatePost(m_LoggedInUser.Birthday);
+                MessageBox.Show("astrologyHoroscopePost");
+                Status postedStatus = m_LoggedInUser.PostStatus(astrologyHoroscopePost);
+                MessageBox.Show("Status Posted! ID: " + postedStatus.Id);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            ///
+        }
+    }
 }
