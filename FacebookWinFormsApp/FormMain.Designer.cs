74d515f2cfd36fe77f3c9c91be7d3edb51f569aa
shanikrauss <70583407+shanikrauss@users.noreply.github.com>
2022-11-26 18:39:52 +0200

    added cover photo and groups listbox

--- FacebookWinFormsApp/FormMain.Designer.cs
+++ FacebookWinFormsApp/FormMain.Designer.cs
@@ -36,15 +36,19 @@
             this.listBoxPages = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonAstrologyHoroscopePost = new System.Windows.Forms.Button();
+            this.listBoxGroups = new System.Windows.Forms.ListBox();
+            this.pictureBoxCover = new System.Windows.Forms.PictureBox();
+            this.labelGroups = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCover)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(1024, 12);
-            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonLogin.Location = new System.Drawing.Point(910, 10);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4);
             this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(226, 35);
+            this.buttonLogin.Size = new System.Drawing.Size(201, 28);
             this.buttonLogin.TabIndex = 36;
             this.buttonLogin.Text = "Login";
             this.buttonLogin.UseVisualStyleBackColor = true;
@@ -52,10 +56,10 @@
             // 
             // buttonLogout
             // 
-            this.buttonLogout.Location = new System.Drawing.Point(1024, 51);
-            this.buttonLogout.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonLogout.Location = new System.Drawing.Point(910, 41);
+            this.buttonLogout.Margin = new System.Windows.Forms.Padding(4);
             this.buttonLogout.Name = "buttonLogout";
-            this.buttonLogout.Size = new System.Drawing.Size(226, 35);
+            this.buttonLogout.Size = new System.Drawing.Size(201, 28);
             this.buttonLogout.TabIndex = 52;
             this.buttonLogout.Text = "Logout";
             this.buttonLogout.UseVisualStyleBackColor = true;
@@ -63,9 +67,10 @@
             // 
             // pictureBoxProfile
             // 
-            this.pictureBoxProfile.Location = new System.Drawing.Point(86, 12);
+            this.pictureBoxProfile.Location = new System.Drawing.Point(76, 10);
+            this.pictureBoxProfile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBoxProfile.Name = "pictureBoxProfile";
-            this.pictureBoxProfile.Size = new System.Drawing.Size(157, 142);
+            this.pictureBoxProfile.Size = new System.Drawing.Size(140, 114);
             this.pictureBoxProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBoxProfile.TabIndex = 53;
             this.pictureBoxProfile.TabStop = false;
@@ -74,19 +79,20 @@
             // listBoxAboutInfo
             // 
             this.listBoxAboutInfo.FormattingEnabled = true;
-            this.listBoxAboutInfo.ItemHeight = 20;
-            this.listBoxAboutInfo.Location = new System.Drawing.Point(12, 198);
+            this.listBoxAboutInfo.ItemHeight = 16;
+            this.listBoxAboutInfo.Location = new System.Drawing.Point(11, 158);
+            this.listBoxAboutInfo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listBoxAboutInfo.Name = "listBoxAboutInfo";
-            this.listBoxAboutInfo.Size = new System.Drawing.Size(302, 84);
+            this.listBoxAboutInfo.Size = new System.Drawing.Size(269, 68);
             this.listBoxAboutInfo.TabIndex = 55;
             this.listBoxAboutInfo.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // labelAbout
             // 
             this.labelAbout.AutoSize = true;
-            this.labelAbout.Location = new System.Drawing.Point(12, 175);
+            this.labelAbout.Location = new System.Drawing.Point(11, 140);
             this.labelAbout.Name = "labelAbout";
-            this.labelAbout.Size = new System.Drawing.Size(56, 20);
+            this.labelAbout.Size = new System.Drawing.Size(45, 16);
             this.labelAbout.TabIndex = 56;
             this.labelAbout.Text = "About:";
             this.labelAbout.Click += new System.EventHandler(this.labelAbout_Click);
@@ -94,37 +100,71 @@
             // listBoxPages
             // 
             this.listBoxPages.FormattingEnabled = true;
-            this.listBoxPages.ItemHeight = 20;
-            this.listBoxPages.Location = new System.Drawing.Point(12, 324);
+            this.listBoxPages.ItemHeight = 16;
+            this.listBoxPages.Location = new System.Drawing.Point(11, 259);
+            this.listBoxPages.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listBoxPages.Name = "listBoxPages";
-            this.listBoxPages.Size = new System.Drawing.Size(302, 164);
+            this.listBoxPages.Size = new System.Drawing.Size(269, 132);
             this.listBoxPages.TabIndex = 57;
             this.listBoxPages.SelectedIndexChanged += new System.EventHandler(this.listBoxPages_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 298);
+            this.label1.Location = new System.Drawing.Point(11, 238);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.Size = new System.Drawing.Size(82, 16);
             this.label1.TabIndex = 58;
             this.label1.Text = "Liked pages";
             // 
             // buttonAstrologyHoroscopePost
             // 
-            this.buttonAstrologyHoroscopePost.Location = new System.Drawing.Point(1073, 107);
+            this.buttonAstrologyHoroscopePost.Location = new System.Drawing.Point(954, 86);
+            this.buttonAstrologyHoroscopePost.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonAstrologyHoroscopePost.Name = "buttonAstrologyHoroscopePost";
-            this.buttonAstrologyHoroscopePost.Size = new System.Drawing.Size(125, 60);
+            this.buttonAstrologyHoroscopePost.Size = new System.Drawing.Size(111, 48);
             this.buttonAstrologyHoroscopePost.TabIndex = 59;
             this.buttonAstrologyHoroscopePost.Text = "Post Astrology Horoscope";
             this.buttonAstrologyHoroscopePost.UseVisualStyleBackColor = true;
             this.buttonAstrologyHoroscopePost.Click += new System.EventHandler(this.buttonAstrologyHoroscopePost_Click);
             // 
+            // listBoxGroups
+            // 
+            this.listBoxGroups.FormattingEnabled = true;
+            this.listBoxGroups.ItemHeight = 16;
+            this.listBoxGroups.Location = new System.Drawing.Point(306, 259);
+            this.listBoxGroups.Name = "listBoxGroups";
+            this.listBoxGroups.Size = new System.Drawing.Size(258, 132);
+            this.listBoxGroups.TabIndex = 60;
+            this.listBoxGroups.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // pictureBoxCover
+            // 
+            this.pictureBoxCover.Location = new System.Drawing.Point(249, 10);
+            this.pictureBoxCover.Name = "pictureBoxCover";
+            this.pictureBoxCover.Size = new System.Drawing.Size(305, 114);
+            this.pictureBoxCover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxCover.TabIndex = 62;
+            this.pictureBoxCover.TabStop = false;
+            this.pictureBoxCover.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // labelGroups
+            // 
+            this.labelGroups.AutoSize = true;
+            this.labelGroups.Location = new System.Drawing.Point(306, 238);
+            this.labelGroups.Name = "labelGroups";
+            this.labelGroups.Size = new System.Drawing.Size(51, 16);
+            this.labelGroups.TabIndex = 63;
+            this.labelGroups.Text = "Groups";
+            // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1263, 1011);
+            this.ClientSize = new System.Drawing.Size(1123, 809);
+            this.Controls.Add(this.labelGroups);
+            this.Controls.Add(this.pictureBoxCover);
+            this.Controls.Add(this.listBoxGroups);
             this.Controls.Add(this.buttonAstrologyHoroscopePost);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBoxPages);
@@ -133,11 +173,12 @@
             this.Controls.Add(this.pictureBoxProfile);
             this.Controls.Add(this.buttonLogout);
             this.Controls.Add(this.buttonLogin);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCover)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,6 +194,9 @@
         private System.Windows.Forms.ListBox listBoxPages;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonAstrologyHoroscopePost;
+        private System.Windows.Forms.ListBox listBoxGroups;
+        private System.Windows.Forms.PictureBox pictureBoxCover;
+        private System.Windows.Forms.Label labelGroups;
     }
 }
 
