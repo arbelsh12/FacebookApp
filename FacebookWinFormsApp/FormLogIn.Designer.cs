53ce1c3c9827f563158766f2c22359edcd8be627
arbelsh12 <70585738+arbelsh12@users.noreply.github.com>
2022-11-30 10:07:46 +0200

    add log in form files

---
+++ FacebookWinFormsApp/FormLogIn.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace BasicFacebookFeatures
+{
+    partial class FormLogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogIn = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelWelcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonLogIn
+            // 
+            this.buttonLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogIn.Location = new System.Drawing.Point(128, 119);
+            this.buttonLogIn.Name = "buttonLogIn";
+            this.buttonLogIn.Size = new System.Drawing.Size(108, 36);
+            this.buttonLogIn.TabIndex = 0;
+            this.buttonLogIn.Text = "Log In";
+            this.buttonLogIn.UseVisualStyleBackColor = true;
+            this.buttonLogIn.Click += new System.EventHandler(this.buttonLogIn_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonExit.Location = new System.Drawing.Point(128, 161);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(108, 36);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelWelcome
+            // 
+            this.labelWelcome.AutoSize = true;
+            this.labelWelcome.Font = new System.Drawing.Font("Miriam Fixed", 14F, System.Drawing.FontStyle.Bold);
+            this.labelWelcome.Location = new System.Drawing.Point(12, 55);
+            this.labelWelcome.Name = "labelWelcome";
+            this.labelWelcome.Size = new System.Drawing.Size(354, 28);
+            this.labelWelcome.TabIndex = 2;
+            this.labelWelcome.Text = "Welcome to our app!";
+            // 
+            // FormLogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(368, 247);
+            this.Controls.Add(this.labelWelcome);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonLogIn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormLogIn";
+            this.Text = "FormLogIn";
+            this.Load += new System.EventHandler(this.FormLogIn_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogIn;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelWelcome;
+    }
+}
