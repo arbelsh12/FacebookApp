dc007e700578e47bb3b98f28cdfad5975d6f4f92
arbelsh12 <70585738+arbelsh12@users.noreply.github.com>
2022-12-03 20:11:59 +0200

    create MockData and MockEvent

---
+++ FacebookAppLogic/MockData.cs
@@ -0,0 +1,34 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookAppLogic
+{
+    public class MockData
+    {
+        public List<MockEvent> r_Events;
+        public List<MockUser> r_Friends;
+
+        public MockData()
+        {
+            r_Events = createMockEvents();
+            r_Friends = createMockFriends();
+        }
+
+        private List<MockEvent> createMockEvents()
+        {
+            List<MockUser> mockFriends = new List<MockUser>();
+            
+            return mockFriends;
+        }
+
+        private List<MockEvent> createMockFriends()
+        {
+            List<MockEvent> mockEvents = new List<MockEvent> ();
+            
+            return mockEvents;
+        }
+    }
+}
---
+++ FacebookAppLogic/MockEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookAppLogic
+{
+    public class MockEvent
+    {
+        private DateTime m_StartTime;
+        public long m_AttendingCount;
+        private long m_InterestedCount;
+        private long m_DeclinedCount;
+        private long m_MaybeCount;
+        
+        public MockEvent(DateTime i_StartTime, long i_AttendingCount, long i_InterestedCount, long i_DeclinedCount, long i_MaybeCount)
+        {
+            m_StartTime = i_StartTime;
+            m_AttendingCount = i_AttendingCount;
+            m_InterestedCount = i_InterestedCount;
+            m_DeclinedCount = i_DeclinedCount;
+            m_MaybeCount = i_MaybeCount;
+        }
+
+        public DateTime StartTime
+        {
+            get { return m_StartTime; }
+        }
+
+        public long AttendingCount { get { return m_AttendingCount; } }
+
+        public long InterestedCount { get { return m_InterestedCount; } }
+
+        public long DeclinedCount { get { return m_DeclinedCount; } }
+
+        public long MaybeCount { get { return m_MaybeCount; } }
+    }
+}
