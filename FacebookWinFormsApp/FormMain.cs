c9151bdb819c8bf4318a70b7ae04bec2182fff42
shanikrauss <70583407+shanikrauss@users.noreply.github.com>
2023-01-03 11:16:03 +0200

    added more threads operations

--- FacebookWinFormsApp/FormMain.cs
+++ FacebookWinFormsApp/FormMain.cs
@@ -242,17 +242,17 @@ namespace BasicFacebookFeatures
                     }
                 case (int)eTab.LikedPages:
                     {
-                        fetchLikedPages();
+                        new Thread(fetchLikedPages).Start();
                         break;
                     }
                 case (int)eTab.Groups:
                     {
-                        fetchGroups();
+                        new Thread(fetchGroups).Start();
                         break;
                     }
                 case (int)eTab.SportTeams:
                     {
-                        fetchSportTeams();
+                        new Thread(fetchSportTeams).Start();
                         break;
                     }
                 case (int)eTab.Events:
@@ -269,13 +269,21 @@ namespace BasicFacebookFeatures
 
         private void fetchLikedPages()
         {
+            var likedPages = m_LoggedInUser.LikedPages;
+
+            flowLayoutPanelPages.Invoke(new Action(() => fetchLikedPagesMainThread(likedPages)));
+            listBoxLikedPages.Invoke(new Action(() => pageBindingSource.DataSource = likedPages));
+        }
+
+        private void fetchLikedPagesMainThread(FacebookObjectCollection<Page> i_LikedPages)
+        {
             if (flowLayoutPanelPages.Controls.Count == 0)
             {
-                if (m_LoggedInUser.LikedPages != null)
+                if (i_LikedPages != null)
                 {
                     try
                     {
-                        foreach (Page page in m_LoggedInUser.LikedPages)
+                        foreach (Page page in i_LikedPages)
                         {
                             addGroupBoxToPanel(flowLayoutPanelPages, page.Name, page.PictureNormalURL);
                         }
@@ -287,23 +295,29 @@ namespace BasicFacebookFeatures
                 }
             }
 
-            pageBindingSource.DataSource = m_LoggedInUser.LikedPages;
-           
             if (flowLayoutPanelPages.Controls.Count == 0)
             {
                 MessageBox.Show("No liked pages to retrieve :(");
             }
         }
 
+
         private void fetchGroups()
+        {
+            var groups = m_LoggedInUser.Groups;
+
+            flowLayoutPanelGroups.Invoke(new Action(() => fetchGroupsMainThread(groups)));
+        }
+
+        private void fetchGroupsMainThread(FacebookObjectCollection<Group> i_Groups)
         {
             if (flowLayoutPanelGroups.Controls.Count == 0)
             {
-                if(m_LoggedInUser.Groups != null)
+                if (i_Groups != null)
                 {
                     try
                     {
-                        foreach (Group group in m_LoggedInUser.Groups)
+                        foreach (Group group in i_Groups)
                         {
                             addGroupBoxToPanel(flowLayoutPanelGroups, group.Name, group.PictureNormalURL);
                         }
@@ -380,13 +394,20 @@ namespace BasicFacebookFeatures
 
         private void fetchSportTeams()
         {
+            var teams = m_LoggedInUser.FavofriteTeams;
+
+            flowLayoutPanelSport.Invoke(new Action(() => fetchSportTeamsMainThread(teams)));
+        }
+
+        private void fetchSportTeamsMainThread(Page[] i_Teams)
+        {
             if (flowLayoutPanelSport.Controls.Count == 0)
             {
-                if(m_LoggedInUser.FavofriteTeams != null)
+                if (i_Teams != null)
                 {
                     try
                     {
-                        foreach (Page team in m_LoggedInUser.FavofriteTeams)
+                        foreach (Page team in i_Teams)
                         {
                             addGroupBoxToPanel(flowLayoutPanelSport, team.Name, team.PictureNormalURL);
                         }
@@ -397,7 +418,7 @@ namespace BasicFacebookFeatures
                     }
                 }
             }
-
+           
             if (flowLayoutPanelSport.Controls.Count == 0)
             {
                 MessageBox.Show("No sport teams to retrieve :(");
